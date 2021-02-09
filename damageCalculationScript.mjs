473e72f2088439e714d8a7f6d2865f1aed40f191
Mini-mat <mateusclnvitorino@hotmail.com>
2021-02-09 03:19:07 -0300

    Turned dice functions into only one class
    This will be helpful for solving the multiple damage dice issue

---
+++ damageCalculationScript.mjs
@@ -0,0 +1,54 @@
+import {dice} from "./dice.mjs";
+
+//Dice variables
+var d20 = new dice(20);
+var d12 = new dice(12);
+var d10 = new dice(10);
+var d8 = new dice(8);
+var d6 = new dice(6);
+var d4 = new dice(4);
+
+//Variables
+var returnVar = "";
+var advantage = "normal";
+var critical = 19;
+var attackBonus = 9;
+var damageDice = d6
+var damageBonus = 6;
+
+
+
+
+//Damage Calculation
+for (var ac = 10; ac <= 32; ac++){
+    var avgDamage = 0;
+    var avgCalculator = 0;
+    var i=0
+    while (i <= 2000000){
+        switch(advantage){
+            case "normal":
+                var natScore = d20.result;
+                break;
+            
+            case "advantage":
+                var natScore = Math.max(d20.result, d20.result);
+                break;
+            
+            case "elven accuracy":
+                var natScore = Math.max(d20.result, d20.result, d20.result);
+                break;
+            
+            case "disavantage":
+                var natScore = Math.min(d20.result, d20.result);
+        }
+        var attackScore = natScore + attackBonus;
+        if (attackScore >= ac && natScore != 1){
+            avgCalculator += (natScore = critical? damageDice.result + damageDice.result + damageBonus : damageDice.result + damageBonus);
+        }
+        i++
+    }
+    avgDamage = Math.round(avgCalculator/2000000);
+    returnVar += `Average damage for ac ${ac}: ${avgDamage} \n`;
+}
+
+console.log(returnVar);
--- dice.mjs
+++ dice.mjs
@@ -3,38 +3,49 @@
 import MersenneTwister from 'mersenne-twister';
 var generator = new MersenneTwister();
 
+//exporting dice
+//export {d100, d20, d12, d10, d8, d6, d4, d3};
+export {dice}
+
 
 //Getting dice functions
-function d100() {
-    return Math.round(generator.random()*99+1);
-}
+// function d100() {
+//     return Math.round(generator.random()*99+1);
+// }
 
-function d20() {
-    return Math.round(generator.random()*19+1);
-}
+// function d20() {
+//     return Math.round(generator.random()*19+1);
+// }
 
-function d12() {
-    return Math.round(generator.random()*11+1);
-}
+// function d12() {
+//     return Math.round(generator.random()*11+1);
+// }
 
-function d10() {
-    return Math.round(generator.random()*9+1);
-}
+// function d10() {
+//     return Math.round(generator.random()*9+1);
+// }
 
-function d8() {
-    return Math.round(generator.random()*7+1);
-}
+// function d8() {
+//     return Math.round(generator.random()*7+1);
+// }
 
-function d6() {
-    return Math.round(generator.random()*6+1);
-}
+// function d6() {
+//     return Math.round(generator.random()*6+1);
+// }
 
-function d4() {
-    return Math.round(generator.random()*6+1);
-}
+// function d4() {
+//     return Math.round(generator.random()*6+1);
+// }
 
-function d3() {
-    return Math.round(generator.random()*2+1);
-}
+// function d3() {
+//     return Math.round(generator.random()*2+1);
+// }
 
-export {d100, d20, d12, d10, d8, d6, d4, d3}
+class dice {
+    constructor(size){
+        this.size = size;
+    }
+    get result(){
+        return Math.round(generator.random()*(this.size-1)+1)
+    }
+}
